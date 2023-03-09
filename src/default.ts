fa46119946be6fb81b401e7b3cf586d64c777d85
zicen <1140377034@qq.com>
2023-03-09 20:40:07 +0800

    update: tips change

--- src/default.ts
+++ src/default.ts
@@ -7,5 +7,5 @@ export const defaultSetting = {
 }
 
 export const defaultMessage = `
-- 部署自 ourongxing 的开源项目，项目远吗 [源码](https://github.com/ourongxing/chatgpt-vercel)。
+- 部署自 ourongxing 的开源项目，[源码](https://github.com/ourongxing/chatgpt-vercel)。
 - <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
